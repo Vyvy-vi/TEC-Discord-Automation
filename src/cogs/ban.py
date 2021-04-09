73f53b30276d27e04404df5667f668c371d092f4
Vyvy-vi <vyom.j@protonmail.com>
2021-04-09 11:14:00 +0530

    refactor: tweaked ban cog

--- src/cogs/ban.py
+++ src/cogs/ban.py
@@ -2,6 +2,7 @@ from discord import Embed
 from discord.ext import commands
 from discord.ext.commands import Context
 
+
 class Bans(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -9,8 +10,9 @@ class Bans(commands.Cog):
     @commands.command()
     @commands.has_any_role("Can Ban")
     async def hammer(self, ctx: Context, reg: str):
-        gather_list = list([member for member in ctx.message.guild.members if member.display_name.startswith(reg)])
-        await ctx.send(embed=Embed(title='Swinging Hammer on...', description='```diff\n- '+ ' | '.join([str(i) for i in gather_list]) +'```'))
+        gather_list = list(
+            [member for member in ctx.message.guild.members if member.display_name.startswith(reg)])
+        await ctx.send(embed=Embed(title='Swinging Hammer on...', description='```diff\n- ' + ' | '.join([str(i) for i in gather_list]) + '```'))
         for i in gather_list:
             await asyncio.sleep(30)
             await ctx.send(f'Banning {str(i)}#{i}')
@@ -19,10 +21,10 @@ class Bans(commands.Cog):
     @commands.command()
     @commands.has_any_role("Can Ban")
     async def gather(self, ctx: Context, reg: str):
-        gather_list = list([[member.name, member.id] for member in ctx.message.guild.members if member.display_name.startswith(reg)])
-        await ctx.send(embed=Embed(description='```fix\n'+ ' | '.join([str(i) for i in gather_list]) +'```'))
+        gather_list = list([[member.name, member.id]
+                            for member in ctx.message.guild.members if member.display_name.startswith(reg)])
+        await ctx.send(embed=Embed(description='```fix\n' + ' | '.join([str(i) for i in gather_list]) + '```'))
 
 
 def setup(bot):
-    bot.add_cog(Bans(commands.Cog))
-
+    bot.add_cog(Bans(bot))
