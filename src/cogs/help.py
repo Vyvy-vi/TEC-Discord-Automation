7d0b14f636bc99fac1b098afaeef43dc5d43883e
Vyvy-vi <vyom.j@protonmail.com>
2021-05-02 02:41:01 +0530

    feat: detailed help[argument] command

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -1,4 +1,4 @@
-from discord import Embed
+from discord import Embed, Color
 from discord.ext import commands
 from discord.ext.commands import Context
 
@@ -9,7 +9,13 @@ For any problems, open an issue on the [Github Repo](https://github.com/Vyvy-vi/
 
 class CustomHelp(commands.HelpCommand):
     def get_command_signature(self, command):
-        return "`%s%s %s`" % (self.clean_prefix, command.qualified_name, command.signature)
+        cmd = "%s%s %s" % (self.clean_prefix, command.qualified_name, command.signature)
+        return f"`{cmd.strip()}`"
+
+    def get_help_text(self, command):
+        cmd = self.get_command_signature(command)
+        return cmd, command.short_doc
+
     # TEC!help
     async def send_bot_help(self, mapping):
         embed = Embed(title="Help Categories",
@@ -26,15 +32,37 @@ class CustomHelp(commands.HelpCommand):
 
    # TEC!help <command>
     async def send_command_help(self, command):
-        await self.context.send("This is help command")
+        embed = Embed(title=self.get_command_signature(command),
+                      color=0xdefb48)
+        embed.add_field(name=f"Help for `{command.name}`",
+                        value=command.short_doc)
+        alias = command.aliases
+        if alias:
+            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
+        embed.set_footer(text='For any more info, dm @Vyvy-vi#5040')
+        await self.context.send(embed=embed)
 
    # TEC!help <group>
     async def send_group_help(self, group):
-        await self.context.send("This is help group")
+        embed = Embed(title=f"Help for Category: `{cog_name}`",
+                      description=HelpDesic + '\nThat group doesn\'t exist',
+                      colour=Color.Red())
+        await self.context.send(embed=embed)
 
    # TEC!help <cog>
     async def send_cog_help(self, cog):
-        await self.context.send("This is help cog")
+        cog_name = getattr(cog, "qualified_name", "No Category")
+        embed = Embed(title=f"Help for Category: `{cog_name}`",
+                      description="Use TEC!help command for more info on a command"
+                      + f'\n\n**{cog.description}**',
+                      colour=0xdefb48)
+        commands = [self.get_help_text(c) for c in await self.filter_commands(cog.walk_commands(), sort=True)]
+        if commands:
+            for cmd in commands:
+                embed.add_field(name=cmd[0], value=cog.description + cmd[1], inline=False)
+        embed.set_footer(text='For any more info, dm @Vyvy-vi#5040')
+        await self.context.send(embed=embed)
+
 
 class Helpers(commands.Cog):
     """Help command and some other helper commands"""
