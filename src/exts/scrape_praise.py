cfe693108664ffb429fec40601b701bc5a0c427c
Vyvy-vi <vyom.j@protonmail.com>
2021-06-19 20:56:25 +0530

    test: fixed a few bugs

--- src/exts/scrape_praise.py
+++ src/exts/scrape_praise.py
@@ -1,6 +1,8 @@
+import re
 import csv
 
 from discord import Embed
+from discord import utils
 from discord.ext import commands
 from discord.ext.commands import Context
 from datetime import datetime
@@ -13,14 +15,14 @@ class Praise(commands.Cog):
 
     @commands.command()
     async def get_praise(self, ctx, after: str = None):
-        await ctx.send('Fetching all Praise...')
+        await ctx.send(f'Fetching all Praise from {ctx.channel.name}...')
         if after:
             dates = [int(char) for char in after.split('-')]
             msgs = await ctx.channel.history(after=datetime(20 + dates[2], dates[1], dates[0])).flatten()
         else:
             msgs = await ctx.channel.history(after=datetime(2021, 6, 4)).flatten()
 
-        clean_msgs = [['To', 'From', 'Reason for Dishing', 'Date', 'Room']]
+        clean_msgs = []
         for msg in msgs:
             if msg.content.startswith('!praise'):
                 for person in msg.mentions:
@@ -28,17 +30,22 @@ class Praise(commands.Cog):
                         [
                             "@" + person.name,
                             msg.author.name + msg.author.discriminator,
-                            msg.content,
+                            re.sub(r'(<@).*?>', '', utils.escape_mentions(msg.content)[8:]).strip(),
                             msg.created_at.strftime("%b-%d-%Y"),
                             msg.channel.name
                         ]
                     )
                     print(f'Adding praise for: {person.name}')
 
-        with open("praise.csv", 'w') as f:
-            writer = csv.writer(f)
-            print('Writing Praise...')
-            writer.writerows(clean_msgs)
+        with open("praise.csv", 'a') as f:
+            try:
+                writer = csv.writer(f)
+                print('Writing Praise...')
+                writer.writerows(clean_msgs)
+            except Exception as e:
+                print(f"Error in writing Praise: {e}")
+
+        print("Praise successfully written!")
 
 def setup(bot):
     bot.add_cog(Praise(bot))
