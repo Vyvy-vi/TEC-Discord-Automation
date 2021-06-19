04cd1b573c92b999f61d397b6b9969339f318e92
Vyvy-vi <vyom.j@protonmail.com>
2021-06-19 19:11:07 +0530

    test: fixed a few bugs

--- src/exts/scrape_praise.py
+++ src/exts/scrape_praise.py
@@ -15,24 +15,25 @@ class Praise(commands.Cog):
     async def get_praise(self, ctx, after: str = None):
         await ctx.send('Fetching all Praise...')
         if after:
-            dates = after.split('-')
+            dates = [int(char) for char in after.split('-')]
             msgs = await ctx.channel.history(after=datetime(20 + dates[2], dates[1], dates[0])).flatten()
         else:
             msgs = await ctx.channel.history(after=datetime(2021, 6, 4)).flatten()
 
         clean_msgs = [['To', 'From', 'Reason for Dishing', 'Date', 'Room']]
         for msg in msgs:
-            for person in msg.mentions:
-                clean_msgs.append(
-                    [
-                        "@" + person.name,
-                        msg.author.name + msg.author.discriminator,
-                        msg.content,
-                        msg.created_at.strftime("%b-%d-%Y"),
-                        msg.channel.name
-                    ]
-                )
-                print(f'Adding praise for: {person.name}')
+            if msg.content.startswith('!praise'):
+                for person in msg.mentions:
+                    clean_msgs.append(
+                        [
+                            "@" + person.name,
+                            msg.author.name + msg.author.discriminator,
+                            msg.content,
+                            msg.created_at.strftime("%b-%d-%Y"),
+                            msg.channel.name
+                        ]
+                    )
+                    print(f'Adding praise for: {person.name}')
 
         with open("praise.csv", 'w') as f:
             writer = csv.writer(f)
